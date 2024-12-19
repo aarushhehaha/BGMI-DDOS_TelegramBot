a7abaace88b3d827b26a3d7760cda9427d6d2976
aarushhehaha <aarushonreddit@gmail.com>
2024-12-19 17:42:10 +0530

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '7582560994:AAFAu-KjmzICOTXiFkSbAJ0IkyFwjwRFxSk'
+ADMIN_ID = '5202129154'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
